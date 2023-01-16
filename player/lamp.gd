c6be8c03756e02150cd1ce30fa09c6a0f00595f5
Antonio Dell'Annunziata <contact@antonio-da.dev>
2023-01-16 22:29:56 +0100

    feat: Add different lamp rotation speeds

--- player/lamp.gd
+++ player/lamp.gd
@@ -3,13 +3,18 @@ extends Node2D
 enum LightMode {CONE, BEAM}
 
 
-@export var movement_radius := 20
+@export var movement_radius := 20:
+	set(new_value):
+		movement_radius = new_value
+		_light_origin.position = Vector2.RIGHT * movement_radius
 @export var max_beam_distance := 32:
 	set(new_value):
 		max_beam_distance = new_value
 		_current_beam_distance = max_beam_distance
+@export var beam_rotation_speed := .5
+@export var cone_rotation_speed := 10
 
-@onready var _light_origin := $LightOrigin
+@onready var _light_origin := $LightOrigin as Node2D
 @onready var _light_cone := $LightOrigin/LightCone
 @onready var _light_beam := $LightOrigin/LightBeam
 @onready var _light_beam_damage_ray := $LightOrigin/LightBeamDamageRay
@@ -30,15 +35,18 @@ func _set_light_mode(new_mode: LightMode):
 				_light_cone.visible = true
 				_light_beam.visible = false
 				_light_beam_damage_ray.enabled = false
+				_rotation_speed = cone_rotation_speed
 			LightMode.BEAM: 
 				_light_cone.visible = false
 				_light_beam.visible = true
 				_light_beam_damage_ray.enabled = true
+				_rotation_speed = beam_rotation_speed
 		_current_mode = new_mode
 
 
 func _ready():
 	_current_mode = LightMode.CONE
+	_light_origin.position = Vector2.RIGHT * movement_radius
 
 
 func _unhandled_input(event):
@@ -52,14 +60,21 @@ func _physics_process(delta):
 	_follow_mouse(delta)
 
 
+var _mouse_pos: Vector2
+var _original_angle: float
+var _target_angle: float
+var _rotation_progress = 0.0
+var _rotation_speed: float
+
 func _follow_mouse(delta):
-	var mouse_pos = get_local_mouse_position()
-	var angle = position.angle_to_point(mouse_pos)
-	_light_origin.rotation = angle
-	
-	if mouse_pos != Vector2.ZERO: 
-		var direction = mouse_pos.normalized()
-		_light_origin.position = direction * movement_radius
+	if _mouse_pos != get_global_mouse_position():
+		_mouse_pos = get_global_mouse_position()
+		_original_angle = rotation
+		_target_angle = global_position.angle_to_point(_mouse_pos)
+		_rotation_progress = 0
+	if _rotation_progress < 1.0:
+		_rotation_progress += delta * _rotation_speed
+		rotation = lerp_angle(_original_angle, _target_angle, ease(_rotation_progress, .5))
 
 
 func _on_light_beam_damage_ray_beam_obstructed(obstruction_position: Vector2):
